fdbb8d9f321a853182c38191f06b12ea253c76eb
Sandro Santilli <strk@kbt.io>
2017-06-07 13:49:38 +0200

    Add real test for create_table_polygon_grid
    Closes #1

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -24,9 +24,73 @@ SET client_min_messages TO WARNING;
 
 CREATE EXTENSION pgtap;
 
-SELECT plan(1);
+SELECT plan(9);
 
 SELECT has_function( 'public', 'create_table_polygon_grid', ARRAY['name','name','name', 'double precision', 'double precision']  );
+
+-- Test create_table_polygon_grid function {
+
+--ERROR:  Table public.test does not exists
+SELECT throws_ok( $$
+  SELECT create_table_polygon_grid('public', 'test', 'g', 10, 10)
+$$, $$Table public.test does not exists$$
+);
+
+CREATE TABLE public.test (g geometry);
+
+--ERROR:  Table public.test geometry column fake does not exists
+SELECT throws_ok( $$
+  SELECT create_table_polygon_grid('public', 'test', 'fake', 10, 10)
+$$, $$Table public.test geometry column fake does not exists$$
+);
+
+--ERROR:  Table 'public'.test column g does not contain any polygons
+SELECT throws_ok( $$
+  SELECT create_table_polygon_grid('public', 'test', 'g', 10, 10)
+$$, $$Table 'public'.test column g does not contain any polygons$$
+);
+
+INSERT INTO public.test (g) VALUES
+('POLYGON((0 0,20 0,20 20,0 20,0 0))');
+
+--ERROR:  Table 'public'.test does not have a unique non-composite, non-null primary key
+SELECT throws_ok( $$
+  SELECT create_table_polygon_grid('public', 'test', 'g', 10, 10)
+$$, $$Table 'public'.test does not have a unique non-composite, non-null primary key$$
+);
+
+ALTER TABLE public.test ADD id serial primary key;
+
+SELECT is( create_table_polygon_grid('public', 'test', 'g', 10, 10)::text,
+  'test_grid', 'create_table_polygon_grid returns test_grid');
+
+-- Sum of areas is about 400 (from 20x20 original)
+SELECT is( round(sum(st_area(geom))), 400::float8,
+  'Sum of geometry areas is 400')
+  FROM public.test_grid;
+
+-- No single area is bigger than 100 (due to 10x10 resolution)
+SELECT is_empty( $$
+  SELECT id,st_area(geom)
+    FROM public.test_grid
+   WHERE round(ST_Area(geom)) > 100
+$$, 'No grid geometry has area bigger than 100' );
+
+-- Symdiff test
+SELECT is( (
+  SELECT ST_AsText(ST_SymDifference(g, (
+    SELECT ST_Union(geom) FROM test_grid
+  )))::text
+  FROM public.test ),
+  'GEOMETRYCOLLECTION EMPTY'::text,
+  'Symdifference between source geom and union of grid geoms is empty'
+);
+
+-- TODO: check that no geometries have an area smaller then
+--       a given tolerance ?
+--       ie: see https://github.com/linz/linz-postgresql-functions/issues/7
+
+-- }
 
 SELECT * FROM finish();
 
